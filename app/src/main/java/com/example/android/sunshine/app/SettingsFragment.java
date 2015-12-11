5fca602a9676f93e428ab6e5c413cebca4805c78
Pulkit Chaudhary <chaudharypulkit93@gmail.com>
2015-12-11 23:53:54 +0530

    Adding SettingsFragment for Settings screen.

---
+++ app/src/main/java/com/example/android/sunshine/app/SettingsFragment.java
@@ -0,0 +1,60 @@
+package com.example.android.sunshine.app;
+
+import android.content.SharedPreferences;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.prefs.PreferenceChangeListener;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    public SettingsFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceManager().getSharedPreferences()
+                              .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getPreferenceManager().getSharedPreferences()
+                              .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    /**
+     * Called when a shared preference is changed, added, or removed. This
+     * may be called even if a preference is set to its existing value.
+     * <p>
+     * <p>This callback will be run on your main thread.
+     *
+     * @param sharedPreferences The {@link SharedPreferences} that received
+     *                          the change.
+     * @param key               The key of the preference that was changed, added, or
+     */
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if(key.equals("pref_temp_units")) {
+            Preference preference = findPreference(key);
+            preference.setSummary(sharedPreferences.getString(key, ""));
+        }
+    }
+}
