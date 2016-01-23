dda3e5d1561660df4c14f5a70cd4d83ddc896cd1
Pulkit Chaudhary <chaudharypulkit93@gmail.com>
2016-01-23 23:03:04 +0530

    Optimizing imports. Final Sunshine commit.

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -20,7 +20,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.example.android.sunshine.app.data.WeatherContract;
 import com.example.android.sunshine.app.sync.SunshineSyncAdapter;
